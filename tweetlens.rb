e48aeb8c24ca2a808d4d82ca92a57d55aa90484d
Reid Beels <mail@reidbeels.com>
2009-03-08 04:48:35 -0400

    Moved site title and tagline into config file

--- tweetlens.rb
+++ tweetlens.rb
@@ -1,25 +1,24 @@
 require 'rubygems'
 require 'open-uri'
 require 'sinatra'
-require 'vendor/simple-rss-1.2/lib/simple-rss'
 require 'vendor/htmlentities-4.0.0/lib/htmlentities'
+require 'vendor/simple-rss-1.2/lib/simple-rss'
+SimpleRSS.item_tags << :image
 
 DEFAULT_CONFIG = {  "cache_max_age"=>300, 
                     "query"=>""
                   }
 
-SimpleRSS.item_tags << :image
+CONFIG = OpenStruct.new(DEFAULT_CONFIG.merge((File.exists?('CONFIG.yml') && YAML::load_file('CONFIG.yml')) || {}))
 
-config = OpenStruct.new(DEFAULT_CONFIG.merge((File.exists?('config.yml') && YAML::load_file('config.yml')) || {}))
-
-querystring = "q=#{CGI::escape(config.query)}"
-querystring += "&lang=#{config.language}" if config.language
-querystring += "&rpp=#{config.count}" if config.count
-querystring += "&geocode=#{config.count}" if config.geocode
+querystring = "q=#{CGI::escape(CONFIG.query)}"
+querystring += "&lang=#{CONFIG.language}" if CONFIG.language
+querystring += "&rpp=#{CONFIG.count}" if CONFIG.count
+querystring += "&geocode=#{CONFIG.count}" if CONFIG.geocode
 
 get '/' do
   @feed = SimpleRSS.parse open("http://search.twitter.com/search.atom?#{querystring}").read.gsub('<link type="image/png"','<image')
-  headers 'Cache-Control' => "public, max-age=#{config.cache_max_age}"
+  headers 'Cache-Control' => "public, max-age=#{CONFIG.cache_max_age}"
   haml :index
 end
 
