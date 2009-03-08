9aee6b6fa012e28b4d33b4d8f7ef191dbe03616f
Reid Beels <mail@reidbeels.com>
2009-03-08 03:46:59 -0400

    Extracted query and cache settings into a configuration file.

--- tweetlens.rb
+++ tweetlens.rb
@@ -4,12 +4,22 @@ require 'sinatra'
 require 'vendor/simple-rss-1.2/lib/simple-rss'
 require 'vendor/htmlentities-4.0.0/lib/htmlentities'
 
+DEFAULT_CONFIG = {  "cache_max_age"=>300, 
+                    "query"=>""
+                  }
+
 SimpleRSS.item_tags << :image
 
+config = OpenStruct.new(DEFAULT_CONFIG.merge((File.exists?('config.yml') && YAML::load_file('config.yml')) || {}))
+
+querystring = "q=#{CGI::escape(config.query)}"
+querystring += "&lang=#{config.language}" if config.language
+querystring += "&rpp=#{config.count}" if config.count
+querystring += "&geocode=#{config.count}" if config.geocode
 
 get '/' do
-  @feed = SimpleRSS.parse open('http://search.twitter.com/search.atom?q=%23afterhours&rpp=20').read.gsub('<link type="image/png"','<image')
-  headers 'Cache-Control' => 'public, max-age=300'
+  @feed = SimpleRSS.parse open("http://search.twitter.com/search.atom?#{querystring}").read.gsub('<link type="image/png"','<image')
+  headers 'Cache-Control' => "public, max-age=#{config.cache_max_age}"
   haml :index
 end
 
